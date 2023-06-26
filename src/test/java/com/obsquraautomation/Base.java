59c22696e55d5b33da35be2be4b36ddd315b2b4c
ShyamaPM <shyamapm94@gmail.com>
2023-06-26 21:27:38 +0530

    first commit

---
+++ src/test/java/com/obsquraautomation/Base.java
@@ -0,0 +1,22 @@
+package com.obsquraautomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver;
+	public void initialiseBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhij\\OneDrive\\Documents\\Eclipse_Workspace\\obsquraautomation\\src\\main\\java\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
+	}
+	
+	public static void main(String args[])
+	{
+		Base base = new Base();
+		base.initialiseBrowser();
+	}
+	
+
+}
